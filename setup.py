38d1bc1f181cea184d4782a463bfac3105a934b8
Arvind Rajan <arvindrajan92@gmail.com>
2024-03-14 23:39:03 +1100

    Updating setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 from setuptools import setup, find_packages
-import codecs
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'A fast automatic number-plate recognition (ANPR) library'
-LONG_DESCRIPTION = ('A package that employs lightweight models (such as YOLO) and library (such as Paddle OCR) for a '
+LONG_DESCRIPTION = ('A package that employs lightweight models (such as YOLOv8) and library (such as Paddle OCR) for a '
                     'fast ANPR')
 
 setup(
@@ -19,10 +12,20 @@ setup(
     author_email="<arvindrajan92@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
-    packages=find_packages(),
+    long_description=LONG_DESCRIPTION,
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=['ultralytics>=8.1.26', 'paddlepaddle>=2.6.0', 'paddleocr>=2.7.0.3'],
     python_requires='>=3.9',
     license='MIT',
-    keywords=['python', 'anpr', 'fast', 'licence plate', 'detection', 'recognition', 'yolo']
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: AGPL-3.0 license",
+        "Operating System :: OS Independent",
+    ],
+    url='https://github.com/arvindrajan92/fastanpr',
+    keywords=[
+        'python', 'anpr', 'fast', 'licence plate', 'number plate', 'detection', 'recognition', 'YOLOv8', 'paddleocr',
+        'paddlepaddle'
+    ]
 )
